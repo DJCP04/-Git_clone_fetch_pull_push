44e615cdd5b398978f796a4a8718aa7a12ccd331
Denis Chiriboga <dcp00@iesemilidarder.com>
2024-01-27 16:23:11 +0100

    Añadimos programa

---
+++ Programa/src/programa/Programa.java
@@ -0,0 +1,13 @@
+
+package programa;
+
+/**
+ *
+ * @author DJ2004
+ */
+public class Programa {
+ public static void main(String[] args) {
+   System.out.println("Aprendre a utilitzar git clone, git fetch i git pull");
+ }
+}
+
